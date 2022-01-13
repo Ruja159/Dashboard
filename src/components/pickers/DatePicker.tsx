9cb20b75b28a3df12085a30df64eb3e0d6e2756e
Aleksandar Rujevic <aleksandar.rujevic@jsguru.io>
2022-01-13 16:37:53 +0100

    quick fix

--- src/components/pickers/DatePicker.tsx
+++ src/components/pickers/DatePicker.tsx
@@ -1,11 +1,12 @@
 import { DatePicker, Space } from 'antd';
+import {AiOutlineLine} from "react-icons/ai";
 
 const { RangePicker } = DatePicker;
 
 export const CustomDatePicker = () => {
     return (
         <>
-            <RangePicker picker="month" className="custom-date-picker" format = "MMM YYYY"/>
+            <RangePicker picker="month" className="custom-date-picker" format = "MMM YYYY" superNextIcon={<AiOutlineLine />}/>
         </>
     )
 
